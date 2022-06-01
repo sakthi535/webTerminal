1e7aa862bd044228b15e3a4f8e701cc888851e2e
sakthi535 <ssakthi20030305@gmail.com>
2022-06-02 00:57:15 +0530

    React Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Terminal from './components/Terminal';
+
+function App() {
+  return (
+    <div className="font-link ">
+      <h1 className='navbar'>Hello World</h1>
+
+      <Terminal/>
+    </div>
+  );
+}
+
+export default App;
